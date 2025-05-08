97d018d21ff2e5fd5b52885775d1ff59f8db013f
IsepMartimOliveira <106027678+IsepMartimOliveira@users.noreply.github.com>
2025-05-08 22:02:18 +0100

    Reduced vulnabilaties

--- src/test/java/PlansTest/model/PlansTest.java
+++ src/test/java/PlansTest/model/PlansTest.java
@@ -3,11 +3,14 @@ package PlansTest.model;
 
 import com.example.psoft_22_23_project.plansmanagement.model.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlansTest {
 
-
+    @Value("${user.testchicouser}")
+    private String userTest;
 
     private Plans createPlan(String name) {
         Name planName = new Name();
@@ -176,7 +179,7 @@ public class PlansTest {
 
         Double aFee = 4.0;
         Double mFee = 12.0;
-        String user = "chico";
+        String user = userTest;
 
         plans.moneyData(0,aFee,mFee,user);
 
