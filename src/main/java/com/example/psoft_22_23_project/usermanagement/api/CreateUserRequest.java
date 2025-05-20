fbc7e78119734e894fb5df5189187719241b5f16
David Ferreira <davidsferreira02@gmail.com>
2025-05-20 18:18:55 +0100

    Some changes to accomplish 2.1.3 requirement

--- src/main/java/com/example/psoft_22_23_project/usermanagement/api/CreateUserRequest.java
+++ src/main/java/com/example/psoft_22_23_project/usermanagement/api/CreateUserRequest.java
@@ -18,7 +18,7 @@ public class CreateUserRequest {
 
     @NotNull
     @NotBlank
-    @Size(min = 12)
+    @Size(min = 12 , max = 24)
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$",
             message = "Password must be at least 12 characters and include at least one uppercase letter, one digit, and one special character")
     private String password;
